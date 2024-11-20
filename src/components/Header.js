bd721533a58d5c71830e5b71a7166c9c11d0d05f
Sadat Ahmed <sadat@sadats-mbp.myfiosgateway.com>
2024-11-20 17:32:32 -0500

    new

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ function Header() {
         <header className="header">
             {/* Replace header title with an icon-sized logo */}
             <img src="/images/logo3.png" alt="JCare Logo" className="header-logo" />
-            <p className="header-subtitle">“REZONING IS THE FIRST STEP IN SELLING A NEIGHBORHOOD TO RICH PEOPLE”</p>
         </header>
     );
 }
